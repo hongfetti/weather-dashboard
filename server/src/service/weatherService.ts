f004d2004a3f4704d934444e6b051e8eaaee5941
hongfetti <ryan.hong1994@gmail.com>
2025-01-27 17:32:13 -0500

    middleware implemented

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,11 +2,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-
+interface Coordinates {
+ latitude: number,
+ longitude: number,
+}
 // TODO: Define a class for the Weather object
 
+
 // TODO: Complete the WeatherService class
-class WeatherService {
+class WeatherService implements Coordinates {
+  name: string;
+  latitude: number;
+  longitude: number;
+
+  constructor (
+    name: string,
+    latitude: number,
+    longitude: number,
+  )
+
+  }
   // TODO: Define the baseURL, API key, and city name properties
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
@@ -25,12 +40,15 @@ class WeatherService {
   // TODO: Complete buildForecastArray method
   // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {
-//   console.log(city)
+  const getWeatherForCity = async (city: string) => {
+  console.log(city)
 
-//   fetch(`${this.baseURL}/data/2.5/weather?q=${city}&appid=${this.apiKey}`)
+  const response = await fetch(`${this.baseURL}/data/2.5/weather?q=${city}&appid=${this.apiKey}`)
 
-//   fetch(`${this.baseURL}/data/2.5/weather?q=${city}&appid=${this.apiKey}`)}
-//   }
+ const todayForecast = await response.json()
+  }
+  
+// 5-day forecast:
+//  const response2 = await fetch(`${this.baseURL}/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${this.apiKey}`)
 
 export default new WeatherService();
